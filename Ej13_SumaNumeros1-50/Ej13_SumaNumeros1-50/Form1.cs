e37b1d52f712e50c25c539e0b5c3a430d37c96bd
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-16 17:55:44 -0600

    Add files via upload

---
+++ Ej13_SumaNumeros1-50/Ej13_SumaNumeros1-50/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej13_SumaNumeros1_50
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SumaNumeros x = new SumaNumeros();
+            int suma = x.Suma();
+            MessageBox.Show("La suma de los numeros del 1 al 50 es: " + suma, "1-50", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Ej13_SumaNumeros1-50/Ej13_SumaNumeros1-50/SumaNumeros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej13_SumaNumeros1_50
+{
+    class SumaNumeros
+    {
+        public int Suma()
+        {
+            int suma = 0;
+            for (int i = 1; i <= 50; i++)
+            {
+                suma = suma + i;
+            }
+            return (suma);
+        }
+    }
+}
